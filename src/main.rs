69d69f6eeb234836226638f524dcfab22cbe930a
automaton641 <automaton641@gmail.com>
2020-05-26 21:48:57 -0500

    update

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use sentidos::*;
+
+fn mouse_input_handler(button: MouseButton, state: ElementState, position: (usize, usize)) {
+    match button {
+        MouseButton::Left => match state {
+            ElementState::Pressed => {
+                println!("left mouse button pressed");
+                println!("position {}, {}", position.0, position.1);
+            }
+            _ => (),
+        },
+        _ => (),
+    }
+}
+
+struct SymbolProgram {}
+
+impl SymbolProgram {
+    fn new() -> SymbolProgram {
+        SymbolProgram {}
+    }
+}
+
+fn main() {
+    let symbol_program = SymbolProgram::new();
+    let mut window = Window::new(symbol_program, "symbol", 1024, 512);
+    window.add_mouse_input_handler(mouse_input_handler);
+    //window.set_user_data(symbol_program);
+    window.show();
+}
